05a0fb1b5da52f52cb3132f744ae62b98a7146d0
Dinesh K <309713913+pkmdinesh@users.noreply.github.com>
2026-08-09 22:48:25 +0530

    Make announcement card styling configurable

--- announcements.js
+++ announcements.js
@@ -3,7 +3,15 @@
 window.OCEAN_WATCH_ANNOUNCEMENTS = [
   {
     active: true,
+    label: 'Notice',
+    symbol: 'i',
     text: 'Official ocean-service notices and important links will appear here.',
+    textColor: '#082f3c',
+    labelColor: '#238269',
+    symbolColor: '#ffffff',
+    accentColor: '#238269',
+    linkColor: '#087f84',
+    backgroundColor: '#e0f2eb',
     linkLabel: 'INCOIS',
     url: 'https://www.incois.gov.in/'
   }
